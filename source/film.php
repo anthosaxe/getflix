6163d533a3c599f13d03ca5806f97e001337bb07
Anthony <dhaene.anthony@hotmail.be>
2024-08-06 17:01:00 +0200

    film page creation

--- source/film.php
+++ source/film.php
@@ -1,20 +1,42 @@
 <!DOCTYPE html>
-
+<html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>FailFlix</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link href="./css/style.css" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style>
+    .group:hover .overlay {
+      opacity: 1;
+    }
+    .color-class {
+      background-color: #4853e4;
+    }
+  </style>
 </head>
 
 <body class="body-color">
-  
-  
 
+  <nav class="color-class border-gray-200">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+      <a href="./main.php" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="./images/cochon.jpg" class="h-8" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FailFlix</span>
+      </a>
+      <div class="flex items-center space-x-6 rtl:space-x-reverse">
+        <a href="login.php" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</a>
+      </div>
+    </div>
+  </nav>
 
+  <div class="container mx-auto px-4 mt-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <?php
+        include "./include/filter_db.php";
+      ?>
+    </div>
+  </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- source/include/filter_db.php
+++ source/include/filter_db.php
@@ -1,40 +1,58 @@
 <?php
 include "connect.php";
 
-function get_by_genre(){
+function get_by_genre($genre) {
     $pdo = connect();
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        if (isset($_POST["genre"]) && !empty($_POST["genre"])) {
-            $genre = $_POST["genre"];
-            $stmt = $pdo->prepare("SELECT movies.*, categories.name AS category_name FROM movies JOIN categories ON movies.category_id = categories.id 
-                WHERE categories.name = :genre");
-            $stmt->bindParam(':genre', $genre, PDO::PARAM_STR);
-            $stmt->execute();
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $results;
-        } else {
-            return ["error" => "Genre not specified"];
-        }
+    $stmt = $pdo->prepare("SELECT movies.*, categories.name AS category_name FROM movies JOIN categories ON movies.category_id = categories.id 
+        WHERE categories.name = :genre");
+    $stmt->bindParam(':genre', $genre, PDO::PARAM_STR);
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
+
+function get_by_name($name) {
+    $pdo = connect();
+    $name = "%$name%";
+    $stmt = $pdo->prepare("SELECT movies.*, categories.name AS category_name FROM movies JOIN categories ON movies.category_id = categories.id 
+        WHERE movies.title LIKE :name");
+    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $results;
+}
+
+function show_films($data){
+    foreach($data as $res){
+        echo "<div class='relative group w-full'>";
+        echo "<img src='".$res['image_url']."' alt='Movie Poster' class='w-full h-full object-cover'>";
+        echo "<div class='absolute inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center opacity-0 overlay transition-opacity duration-300'>";
+        echo "<div class='text-center text-white'>";
+        echo "<h2 class='text-2xl font-bold'>".$res['title']."</h2>";
+        echo "<p class='mt-2 text-lg'>Note: ".$res['rating']."</p>";
+        echo "</div>";
+        echo "</div>";
+        echo "</div>";
+      }
+}
+
+$results = [];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['genre']) && !empty($_POST['genre'])) {
+        $genre = $_POST['genre'];
+        echo '<h1 class="text-5xl font-extrabold dark:text-white">'.$genre.'</h1>';
+        $results = get_by_genre($genre);
+        show_films($results);
+    } elseif (isset($_POST['name']) && !empty($_POST['name'])) {
+        $name = $_POST['name'];
+        echo '<h1 class="text-5xl font-extrabold dark:text-white"> Recherche pour : '.$Name.'</h1>';
+        $results = get_by_name($name);
+        show_films($results);
+    } else {
+        $results = ["error" => "Search type or value not specified"];
     }
 }
 
-
-function get_by_name(){
-    $pdo = connect();
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
-        if (isset($_POST["name"]) && !empty($_POST["name"])) {
-            $name = $_POST["name"];
-            $name = "%$name%";
-            $stmt = $pdo->prepare("SELECT movies.*, categories.name AS category_name FROM movies JOIN categories ON movies.category_id = categories.id 
-            WHERE movies.title LIKE :name");
-            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-            $stmt->execute();
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $results;
-        } else {
-            return ["error" => "Name not specified"];
-        }
-    }
-}
 
 
